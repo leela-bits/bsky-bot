2f0afa894e31ee7ff9e6afb30c188ea9e4eebc1e
Leela Bits <leela-bits@proton.me>
2026-08-15 02:02:41 -0400

    Lots of refactoring

--- lexicons/README.md
+++
@@ -1,6 +0,0 @@
-# lexicon sources
-
-this directory contains lexicon documents pulled from the following sources:
-
-- https://github.com/bluesky-social/atproto.git (ref: main)
-  - commit: b31485ec5012f543fd60304df9a1684fac4e3dea
--- src/index.ts
+++ src/index.ts
@@ -1,186 +1,201 @@
-import dotenv from 'dotenv';
-import { Client, ClientResponseError, ClientValidationError, ok } from '@atcute/client';
-import { PasswordSession } from '@atcute/password-session';
-import { is } from '@atcute/lexicons';
-import { mainSchema as NotifUpdateSeen } from '@atcute/bluesky/types/app/notification/updateSeen';
-import { mainSchema as FeedGetPostThread } from '@atcute/bluesky/types/app/feed/getPostThread';
-import { mainSchema as Post } from '@atcute/bluesky/types/app/feed/post';
-import { threadViewPostSchema as ThreadViewPost } from '@atcute/bluesky/types/app/feed/defs';
-import { publishThread } from '@atcute/bluesky-threading';
-import cron from 'node-cron';
 import process from 'node:process';
+import {
+  AppBskyFeedDefs,
+  AppBskyFeedGetPostThread,
+  AppBskyFeedPost,
+  AppBskyNotificationUpdateSeen,
+} from '@atcute/bluesky';
+import { publishThread } from '@atcute/bluesky-threading';
+import { Client, ClientResponseError, ClientValidationError, ok } from '@atcute/client';
+import { is } from '@atcute/lexicons';
+import { PasswordSession } from '@atcute/password-session';
+import dotenv from 'dotenv';
+import cron from 'node-cron';
 
 // log error information to the console
 function logError(error: unknown) {
-    if (error instanceof ClientValidationError) {
-        console.error(`client validation error: ${error.message}`);
-    } else if (error instanceof ClientResponseError) {
-        console.error(`client response error: ${error.status} ${error.error} ${error.description}`);
-    } else {
-        console.error('unexpected error: ', error);
-    }
+  if (error instanceof ClientValidationError) {
+    console.error(`client validation error: ${error.message}`);
+  } else if (error instanceof ClientResponseError) {
+    console.error(`client response error: ${error.status} ${error.error} ${error.description}`);
+  } else {
+    console.error('unexpected error: ', error);
+  }
 }
 
 // check to see if anyone mentioned us
 async function checkMentions(session: PasswordSession) {
-    // create a client with the authenticated session
-    const client = new Client({
-        handler: session,
-    });
+  // create a client with the authenticated session
+  const client = new Client({
+    handler: session,
+  });
 
-    let next: string | undefined;
+  let next: string | undefined;
 
-    do {
-        try {
-            // get a list of all mention notifications
-            console.log('getting list of all mention notifications');
-            const { cursor, notifications } = await ok(
-                client.get('app.bsky.notification.listNotifications', {
-                    params: {
-                        reasons: ['mention'],
-                        limit: 20,
-                        cursor: next,
-                    },
-                }),
-            );
+  do {
+    try {
+      // get a list of all mention notifications
+      console.log('getting list of all mention notifications');
+      const { cursor, notifications } = await ok(
+        client.get('app.bsky.notification.listNotifications', {
+          params: {
+            reasons: ['mention'],
+            limit: 20,
+            cursor: next,
+          },
+        }),
+      );
 
-            next = cursor;
+      next = cursor;
 
-            console.log(
-                `${notifications.length} notification${notifications.length != 1 ? 's' : ''} retrieved`,
-            );
+      console.log(
+        `${notifications.length} notification${notifications.length !== 1 ? 's' : ''} retrieved`,
+      );
 
-            // process each mention that hasn't been read yet
-            for (const notif of notifications) {
-                if (notif.reason !== 'mention') {
-                    continue;
-                }
-
-                // if the notification has been read then we are done reading
-                if (notif.isRead) {
-                    console.log('all notifications have been read');
-                    next = '';
-                    break;
-                }
-
-                try {
-                    // get the post thread that we were mentioned in
-                    console.log('getting thread that we were mentioned in');
-                    const { thread } = await ok(
-                        client.call(FeedGetPostThread, {
-                            params: { uri: notif.uri },
-                        }),
-                    );
-
-                    // if this is a valid post (i.e. not blocked or missing)
-                    if (is(ThreadViewPost, thread) && is(Post, thread.post.record)) {
-                        const mentionText = thread.post.record.text;
-                        const createdAt = thread.post.record.createdAt;
-
-                        console.log(
-                            `@${notif.author.handle} mentioned us at ${createdAt}: "${mentionText}"`,
-                        );
-
-                        // reply to the post that mentioned us!
-                        console.log('replying to the post that mentioned us');
-                        await publishThread(client, {
-                            author: session.did,
-                            reply: notif.uri,
-                            posts: [
-                                {
-                                    content: {
-                                        text: 'Hi!',
-                                    },
-                                },
-                            ],
-                        });
-
-                        try {
-                            // update the notifications seen time
-                            console.log('updating notifications seen time');
-                            const seenAt = new Date(createdAt);
-                            seenAt.setTime(seenAt.getTime() + 1);
-                            await ok(
-                                client.call(NotifUpdateSeen, {
-                                    input: {
-                                        seenAt: seenAt.toISOString(),
-                                    },
-                                }),
-                            );
-                        } catch (error) {
-                            logError(error);
-                        }
-                    }
-                } catch (error) {
-                    logError(error);
-                }
-            }
-        } catch (error) {
-            logError(error);
+      // process each mention that hasn't been read yet
+      for (const notif of notifications) {
+        if (notif.reason !== 'mention') {
+          continue;
         }
-    } while (next && next !== '');
+
+        // if the notification has been read then we are done reading
+        if (notif.isRead) {
+          console.log('all notifications have been read');
+          next = '';
+          break;
+        }
+
+        try {
+          // get the post thread that we were mentioned in
+          console.log('getting thread that we were mentioned in');
+          const { thread } = await ok(
+            client.call(AppBskyFeedGetPostThread, {
+              params: { uri: notif.uri },
+            }),
+          );
+
+          // if this is a valid post (i.e. not blocked or missing)
+          if (
+            is(AppBskyFeedDefs.threadViewPostSchema, thread) &&
+            is(AppBskyFeedPost.mainSchema, thread.post.record)
+          ) {
+            const mentionText = thread.post.record.text;
+            const createdAt = thread.post.record.createdAt;
+
+            console.log(`@${notif.author.handle} mentioned us at ${createdAt}: "${mentionText}"`);
+
+            // reply to the post that mentioned us!
+            console.log('replying to the post that mentioned us');
+            await publishThread(client, {
+              author: session.did,
+              reply: notif.uri,
+              posts: [
+                {
+                  content: {
+                    text: 'Hi!',
+                  },
+                },
+              ],
+            });
+
+            try {
+              // update the notifications seen time
+              console.log('updating notifications seen time');
+              const seenAt = new Date(createdAt);
+              seenAt.setTime(seenAt.getTime() + 1);
+              await ok(
+                client.call(AppBskyNotificationUpdateSeen, {
+                  input: {
+                    seenAt: seenAt.toISOString(),
+                  },
+                }),
+              );
+            } catch (error) {
+              logError(error);
+            }
+          }
+        } catch (error) {
+          logError(error);
+        }
+      }
+    } catch (error) {
+      logError(error);
+    }
+  } while (next && next !== '');
 }
 
 // main entry point for the bot
 async function main() {
-    console.log('started');
+  console.log('started');
 
-    // get the bot credentials from the environment variables
-    const service = process.env.BOT_PDS!;
-    const identifier = process.env.BOT_HANDLE!;
-    const password = process.env.BOT_PASSWORD!;
+  // get the bot credentials from the environment variables
+  const service = process.env.BOT_PDS ?? 'https://bsky.social';
+  const identifier = process.env.BOT_HANDLE;
+  const password = process.env.BOT_PASSWORD;
 
-    // authenticate using the credentials
-    console.log('authenticating');
-    const session = await PasswordSession.login(
-        {
-            service,
-            identifier,
-            password,
-        },
-        {
-            onDelete: () => {
-                console.log('session deleted');
-            },
-            onUpdate: () => {
-                console.log('session updated');
-            },
-        },
-    );
+  // make sure we have an identifier
+  if (!identifier) {
+    console.error('BOT_HANDLE environment variable is missing');
+    process.exit(1);
+  }
 
-    const task = cron.createTask(
-        '*/10 * * * * *',
-        async () => {
-            // check to see if anyone has mentioned us
-            console.log('checking for mentions');
-            await checkMentions(session);
-        },
-        { noOverlap: true, startTimeout: 1000 },
-    );
+  // make sure we have a password
+  if (!password) {
+    console.error('BOT_PASSWORD environment variable is missing');
+    process.exit(1);
+  }
 
-    // shutdown stops all the background tasks and logout of the session
-    async function shutdown() {
-        console.log('stopping task scheduler');
-        await Promise.all([...cron.getTasks().values()].map((t) => t.destroy()));
+  // authenticate using the credentials
+  console.log('authenticating');
+  const session = await PasswordSession.login(
+    {
+      service,
+      identifier,
+      password,
+    },
+    {
+      onDelete: () => {
+        console.log('session deleted');
+      },
+      onUpdate: () => {
+        console.log('session updated');
+      },
+    },
+  );
 
-        console.log('logging out');
-        await session.logout();
-        process.exit(0);
-    }
+  const task = cron.createTask(
+    '*/10 * * * * *',
+    async () => {
+      // check to see if anyone has mentioned us
+      console.log('checking for mentions');
+      await checkMentions(session);
+    },
+    { noOverlap: true, startTimeout: 1000 },
+  );
 
-    process.once('SIGINT', shutdown);
-    process.once('SIGTERM', shutdown);
+  // shutdown stops all the background tasks and logout of the session
+  async function shutdown() {
+    console.log('stopping task scheduler');
+    await Promise.all([...cron.getTasks().values()].map((t) => t.destroy()));
 
-    task.once('task:started', () => {
-        console.log('task scheduler started');
-    });
+    console.log('logging out');
+    await session.logout();
+    process.exit(0);
+  }
 
-    task.once('task:stopped', () => {
-        console.log('task scheduler stopped');
-    });
+  process.once('SIGINT', shutdown);
+  process.once('SIGTERM', shutdown);
 
-    console.log('starting task scheduler');
-    task.start();
+  task.once('task:started', () => {
+    console.log('task scheduler started');
+  });
+
+  task.once('task:stopped', () => {
+    console.log('task scheduler stopped');
+  });
+
+  console.log('starting task scheduler');
+  task.start();
 }
 
 console.log('starting');
@@ -190,7 +205,7 @@ dotenv.config({ quiet: true });
 
 // run the bot
 await main().catch((error) => {
-    logError(error);
+  logError(error);
 });
 
 console.log('running');
